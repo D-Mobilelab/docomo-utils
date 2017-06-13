4b4f2d50bac908f9fc648d84341557cabca402bd
eatsjobs <pasqualemangialavori@docomodigital.com>
2017-06-13 17:49:09 +0200

    Update docs for JSONPReqiest

--- src/JSONPRequest.js
+++ src/JSONPRequest.js
@@ -5,10 +5,9 @@ import queryfy from './queryfy';
  * The promise will be reject after 3s if the url fail to respond
  *
  * @example
- * <pre>
- * request = new JSONPRequest("http://www.someapi.com/asd?somequery=1");
- * request.then((data) => {});
- * </pre>
+ * const request = new JSONPRequest("http://www.someapi.com/asd?somequery=1");
+ * request.promise.then((data) => {});
+ * request.prom.then((data) => {});
  * @param {String} url - the url with querystring but without &callback at the end or &function
  * @param {Number} timeout - ms range for the response
  * @return {Promise<Object|String>}
@@ -20,7 +19,7 @@ export default function JSONPRequest(url, timeout = 3000) {
   if (window.document) {
     const ts = Date.now();
     self.scriptTag = window.document.createElement('script');
-        // url += '&callback=window.__jsonpHandler_' + ts;
+    // url += '&callback=window.__jsonpHandler_' + ts;
     let _url = '';
     if (url && url !== '') {
       _url = queryfy(url, { callback: `window.__jsonpHandler_${ts}` });
@@ -29,8 +28,7 @@ export default function JSONPRequest(url, timeout = 3000) {
     self.scriptTag.src = _url;
     self.scriptTag.type = 'text/javascript';
     self.scriptTag.async = true;
-
-    self.prom = new Promise((resolve, reject) => {
+    const promise = new Promise((resolve, reject) => {
       const functionName = `__jsonpHandler_${ts}`;
       window[functionName] = function handler(data) {
         self.called = true;
@@ -47,6 +45,8 @@ export default function JSONPRequest(url, timeout = 3000) {
         }
       }, self.timeout);
     });
+    self.prom = promise;
+    self.promise = promise;
     // the append start the call
     window.document.getElementsByTagName('head')[0].appendChild(self.scriptTag);
   }
